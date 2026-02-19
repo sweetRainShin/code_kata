e99e418a9712319db4671a16737bd31a16be652b
sweetRainShin <servdb99@gmail.com>
2026-02-19 10:54:27 +0900

    2026-02-13, 2026-02-19

---
+++ code-kata/src/2026-02-19/overPainting.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int overPainting(int n, int m, int[] section) {
+        int answer = 0;
+        int lastPainted = 0;
+
+        for (int s : section) {
+            if (s > lastPainted) {
+                answer++;
+                lastPainted = s + m - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public int solution(int n, int m, int[] section) {
+        int answer = overPainting(n, m, section);
+        return answer;
+    }
+}
